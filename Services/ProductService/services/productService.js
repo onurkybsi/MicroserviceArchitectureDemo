788cb465e4c98a9b1aa3106b3f92b2a8a3872968
onurkybsi <onurbpm@outlook.com>
2020-11-29 23:49:49 +0300

    getAllProducts method bug fix

--- Services/ProductService/services/productService.js
+++ Services/ProductService/services/productService.js
@@ -1,8 +1,6 @@
 const productCollection = require("../data/productCollection");
 
-const getAllProducts = async ()
-  => await productCollection.find();
-
+const getAllProducts = async () => await productCollection.find();
 
 async function GetList(call, callback) {
   let products = await getAllProducts();
