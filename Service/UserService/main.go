c4bca7e388bf73ab55275862b8a7d6ba141bbef9
onurkybsi <onurbpm@outlook.com>
2021-03-04 02:03:11 +0300

    HelloWorld from UserService

--- Service/UserService/main.go
+++ Service/UserService/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
 	fmt.Println("Hello from UserService !")
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
 }
